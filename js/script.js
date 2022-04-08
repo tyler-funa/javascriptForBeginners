5178be4e6f3948154ad8cf4a251ab9e5381f2ae9
Tyler Funa <funa2695@gmail.com>
2022-04-07 18:33:41 -0700

    finished day 2 challenge

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ console.log("sushi");
 // I'm a single line comment
 /* I am
 a multi line comment */
+
+console.log(2);
+console.log(typeof(2));
+console.log(10n);
+console.log(typeof(10n));
+
+function wordLength(string) {
+  return string.length;
+}
+
+const result = wordLength("Supercalifragilisticexpialidocious");
+
+console.log(result);
