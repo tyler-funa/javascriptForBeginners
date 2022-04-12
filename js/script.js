e390197112ca20fe53f6cfe51896e15c6f67451d
Tyler Funa <funa2695@gmail.com>
2022-04-12 16:09:58 -0700

    comment some stuffs

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
 
-// results
-const resultWordLength = wordLength("Supercalifragilisticexpialidocious");
-
-
-// console logs
-console.log('Tyler Funa');
-console.log('sushi');
-console.log(2);
-console.log(typeof(2));
-console.log(10n);
-console.log(typeof(10n));
-console.log(resultWordLength);
-
-
-
-
-/* functions */
-function wordLength(string) {
-  return string.length;
-}
+// // results
+// const resultWordLength = wordLength("Supercalifragilisticexpialidocious");
+// // console logs
+// console.log('Tyler Funa');
+// console.log('sushi');
+// console.log(2);
+// console.log(typeof(2));
+// console.log(10n);
+// console.log(typeof(10n));
+// console.log(resultWordLength);
+// /* functions */
+// function wordLength(string) {
+//   return string.length;
+// }
 
 
 
