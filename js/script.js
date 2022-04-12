185e94fea59addf4598bb4b462c5dcad1ddf1546
Tyler Funa <funa2695@gmail.com>
2022-04-12 16:08:04 -0700

    begin of day commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-console.log("Tyler Funa");
-console.log("sushi");
 
-// I'm a single line comment
-/* I am
-a multi line comment */
+// results
+const resultWordLength = wordLength("Supercalifragilisticexpialidocious");
 
+
+// console logs
+console.log('Tyler Funa');
+console.log('sushi');
 console.log(2);
 console.log(typeof(2));
 console.log(10n);
 console.log(typeof(10n));
+console.log(resultWordLength);
 
+
+
+
+/* functions */
 function wordLength(string) {
   return string.length;
 }
 
-const result = wordLength("Supercalifragilisticexpialidocious");
 
-console.log(result);
+
